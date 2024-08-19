29ec7976653edc11c224ac7f5c96f11086ea2d56
SilasHakuzwimana <hakusilas@gmail.com>
2024-08-19 15:44:35 +0200

    pushing user_input.py file to github

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Type your location: ')
+
+# Using f-string for formatting
+print(f'Hello {name}, you are {age} years old and live in {location}.')
